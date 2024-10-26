20455b44a05665db0fa8fdf54f23125cbb31fa5e
webhostdevs <webhostdev59@gmail.com>
2024-10-26 16:00:18 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -104,7 +104,7 @@ export default HomePage;
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 
 
 const HomePage = () => {
