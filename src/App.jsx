d7e8665cd7e4cc40248fcf38d73b175c60205552
webhostdevs <webhostdev59@gmail.com>
2024-10-26 20:33:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,4 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import React, { useState } from 'react';
-// import HomePage from './components/HomePage';
-// import VendorDetails from './components/VendorDetails';
-// import Navbar from './components/Navbar';
-// import AuthForm from './components/AuthForm';
-// import './App.css';
 
-// function App() {
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [showAuthForm, setShowAuthForm] = useState(false);
-
-//   // Toggle the visibility of the AuthForm modal
-//   const toggleAuthForm = () => {
-//     setShowAuthForm(!showAuthForm);
-//   };
-
-//   return (
-//     <Router>
-//       <div className="bg-white min-h-screen w-full">
-//         {/* Navbar with toggleAuthForm passed as a prop */}
-//         <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} toggleAuthForm={toggleAuthForm} />
-
-//         {/* Routes */}
-//         <Routes>
-//           <Route path="/" element={<HomePage searchTerm={searchTerm} />} />
-//           <Route path="/vendor/:id" element={<VendorDetails />} />
-//         </Routes>
-
-//         {/* AuthForm Modal */}
-//         {showAuthForm && (
-//           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-//             <div className="bg-white p-6 rounded-lg shadow-lg">
-//               <AuthForm onClose={toggleAuthForm} />
-//               {/* Remove the close button here since we have the 'X' in AuthForm */}
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -50,6 +7,7 @@ import HomePage from './components/HomePage';
 import VendorDetails from './components/VendorDetails';
 import Navbar from './components/Navbar';
 import AuthForm from './components/AuthForm';
+import ContactUs from './components/ContactUs';
 import './App.css';
 
 function App() {
