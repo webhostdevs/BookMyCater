d273602cc5c35000343335a98e565b1ae516ee60
webhostdevs <webhostdev59@gmail.com>
2024-10-26 13:05:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,13 +47,13 @@ function Navbar({ searchTerm, setSearchTerm, toggleAuthForm, isLoggedIn, setLogg
       {/* Right - Search and Navigation */}
       <div className="flex items-center space-x-4">
         <div className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
-          <input
+{/*           <input
             type="text"
             placeholder="Location..."
             className="p-2 w-64 border-none focus:outline-none focus:ring-0 bg-gray-100"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-          />
+          /> */}
           <button className="bg-gray-600 text-white px-4 py-2">Search</button>
         </div>
         <Link to="/" className="text-gray-700 hover:text-black transition">Home</Link>
