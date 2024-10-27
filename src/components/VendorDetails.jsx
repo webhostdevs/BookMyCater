9fc1c249a1a2f6ed02398b08e424ffb7906cc904
webhostdevs <webhostdev59@gmail.com>
2024-10-27 19:21:46 +0530

    Update VendorDetails.jsx

--- src/components/VendorDetails.jsx
+++ src/components/VendorDetails.jsx
@@ -209,7 +209,7 @@ const VendorDetails = () => {
   <p className="text-lg font-medium text-gray-800 mb-4">
     About Seasons Catering Services - {vendor.business_address}
   </p>
-  <div className="text-gray-700 space-y-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-12 ">
+  <div className="text-gray-700 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-12 ">
     <div>
     <p>
       <b>Dietary Options:</b>
