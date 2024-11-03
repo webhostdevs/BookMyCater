734636a7f6af5c28f0a8cbd9d34414133435ffbc
webhostdevs <webhostdev59@gmail.com>
2024-11-03 22:38:45 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar({ searchTerm, setSearchTerm, toggleAuthForm, isLoggedIn, setLogg
         <Link to="/ContactUs" className="text-gray-700 hover:text-black transition">
           Contact
         </Link>  
-        <Link to="/admin" className="text-gray-700 hover:text-black transition">
+        <Link to="https://bookmycater.freewebhostmost.com/admin.html" className="text-gray-700 hover:text-black transition">
               NewVendor
             </Link> 
         {/* Login/Logout Button */}
