c7877a9973841b412a6f962b86902abbb67cee5c
webhostdevs <webhostdev59@gmail.com>
2024-11-12 00:24:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <Auth0Provider
+  //   domain="dev-ewda4aowim00fxep.us.auth0.com"
+  //   clientId="YIJjRXFRaguYdJtGRgQpFIucPQ6uRNcl"
+  //   authorizationParams={{
+  //     redirect_uri: window.location.origin
+  //   }}
+  // >
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  // </Auth0Provider>
+  <Auth0Provider
+  domain="dev-ewda4aowim00fxep.us.auth0.com"
+  clientId="YIJjRXFRaguYdJtGRgQpFIucPQ6uRNcl"
+  redirectUri={window.location.origin}
+  cacheLocation="localstorage"
+>
+  <App />
+</Auth0Provider>,
+// document.getElementById('root')
 )
